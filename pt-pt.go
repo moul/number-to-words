dafa5ad098d822b1033b4e1e5ec5ea216ada4e0d
Pedro Simoes <p.simoes@campus.fct.unl.pt>
2017-10-21 21:44:08 +0100

    uhmm... need to think about this

--- pt-pt.go
+++ pt-pt.go
@@ -1,14 +1,18 @@
 package ntw
 
+import (
+	"fmt"
+	"math"
+)
+
 var portugueseMegasSingular = []string{"", "mil", "milhão", "mil milhões", "bilião"}
 var portugueseMegasPlural = []string{"", "mil", "milhões", "mil milhões", "bilhões"}
 var portugueseUnitsAdjectives = []string{"", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"}
 var portugueseUnits = []string{"", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"}
-var portugueseHundreds = []string{"", "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos", "cento"}
+var portugueseHundreds = []string{"", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos", "cento"}
 var portugueseTens = []string{"", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"}
 var portugueseTeens = []string{"dez", "onze", "doze", "treze", "catorze", "quinze", "dezasseis", "dezasete", "dezoito", "dezanove"}
 
-// IntegerToportuguese converts an integer to portuguese words
 func IntegerToPortuguese_PT(input int) string {
 	//log.Printf("Input: %d\n", input)
 	words := []string{}
@@ -17,5 +21,73 @@ func IntegerToPortuguese_PT(input int) string {
 		words = append(words, "menos")
 		input *= -1
 	}
+	fmt.Printf("%d  %f\n", input, math.Floor(math.Log10(math.Abs(float64(input))))+1)
+
+	triplets := integerToTriplets(input)
+	fmt.Println(triplets)
+	for idx := len(triplets) - 1; idx >= 0; idx-- {
+		hundreds := triplet / 100 % 10
+		tens := triplet / 10 % 10
+		units := triplet % 10
+
+	}
+	// split integer in triplets
+	return Join(words, "")
+
+}
+
+// JOIN From strings.Join
+// following robs pike advice from golang proverbs "A little copying is better than a little dependency."
+func Join(a []string, sep string) string {
+
+	switch len(a) {
+
+	case 0:
+
+		return ""
+
+	case 1:
+
+		return a[0]
+
+	case 2:
+
+		// Special case for common small values.
+
+		// Remove if golang.org/issue/6714 is fixed
+
+		return a[0] + sep + a[1]
+
+	case 3:
+
+		// Special case for common small values.
+
+		// Remove if golang.org/issue/6714 is fixed
+
+		return a[0] + sep + a[1] + sep + a[2]
+
+	}
+
+	n := len(sep) * (len(a) - 1)
+
+	for i := 0; i < len(a); i++ {
+
+		n += len(a[i])
+
+	}
+
+	b := make([]byte, n)
+
+	bp := copy(b, a[0])
+
+	for _, s := range a[1:] {
+
+		bp += copy(b[bp:], sep)
+
+		bp += copy(b[bp:], s)
+
+	}
+
+	return string(b)
 
 }
