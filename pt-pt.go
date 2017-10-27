75265bc57c5d74957e2d1a586b1144ae166001ee
Pedro Simoes <p.simoes@campus.fct.unl.pt>
2017-10-27 01:59:46 +0100

    following go proverb does not make sense since other languages use strings library.

--- pt-pt.go
+++ pt-pt.go
@@ -2,6 +2,7 @@ package ntw
 
 import (
 	"fmt"
+	"strings"
 )
 
 var portugueseMegasSingular = []string{"", "mil", "milhão", "mil milhões", "bilião"}
@@ -10,62 +11,6 @@ var portugueseUnits = []string{"", "um", "dois", "três", "quatro", "cinco", "se
 var portugueseHundreds = []string{"", "cem", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos", "cento"}
 var portugueseTens = []string{"", "dez", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"}
 var portugueseTeens = []string{"dez", "onze", "doze", "treze", "catorze", "quinze", "dezasseis", "dezasete", "dezoito", "dezanove"}
-
-// JOIN From strings.Join
-// following robs pike advice from golang proverbs "A little copying is better than a little dependency."
-func Join(a []string, sep string) string {
-
-	switch len(a) {
-
-	case 0:
-
-		return ""
-
-	case 1:
-
-		return a[0]
-
-	case 2:
-
-		// Special case for common small values.
-
-		// Remove if golang.org/issue/6714 is fixed
-
-		return a[0] + sep + a[1]
-
-	case 3:
-
-		// Special case for common small values.
-
-		// Remove if golang.org/issue/6714 is fixed
-
-		return a[0] + sep + a[1] + sep + a[2]
-
-	}
-
-	n := len(sep) * (len(a) - 1)
-
-	for i := 0; i < len(a); i++ {
-
-		n += len(a[i])
-
-	}
-
-	b := make([]byte, n)
-
-	bp := copy(b, a[0])
-
-	for _, s := range a[1:] {
-
-		bp += copy(b[bp:], sep)
-
-		bp += copy(b[bp:], s)
-
-	}
-
-	return string(b)
-
-}
 
 func IntegerToPortuguesePT(input int) string {
 	//log.Printf("Input: %d\n", input)
@@ -169,6 +114,6 @@ func IntegerToPortuguesePT(input int) string {
 		}
 	}
 
-	return Join(words, " ")
+	return strings.Join(words, " ")
 
 }
